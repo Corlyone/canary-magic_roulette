46efb40f94fc89a6bfdbff6c42b97d74a017e8ad
Nestor Ortega (NvSo) <nvsoonline@gmail.com>
2023-05-28 06:40:50 -0500

    translate strings

--- magic_roulette/lib/core/strings.lua
+++ magic_roulette/lib/core/strings.lua
@@ -6,16 +6,16 @@
 ]]
 
 return {
-	LOAD_CHANCE_MINMAX_WARNING = 'Item with id %d cannot be less than 0.01%% or greater than 100%%.',
-	PRECISE_DROP_WARNING = '%d is not precise drop, result: %s%%',
+	LOAD_CHANCE_MINMAX_WARNING = 'O item com o ID %d não pode ser menor que 0.01%% ou maior que 100%%.',
+	PRECISE_DROP_WARNING = '%d não é uma queda precisa, resultado: %s%%',
 
-	SLOT_NOT_IMPLEMENTED_YET = 'Slot not implemented yet.',
+	SLOT_NOT_IMPLEMENTED_YET = 'Slot ainda não implementado.',
 
-	WAIT_TO_SPIN = 'Wait to spin.',
-	GENERATE_REWARD_FAILURE = 'Something is wrong, contact the administrator.',
-	NEEDITEM_TO_SPIN = 'You need %dx %s to spin.',
+	WAIT_TO_SPIN = 'Aguarde para girar.',
+	GENERATE_REWARD_FAILURE = 'Algo deu errado, entre em contato com o administrador.',
+	NEEDITEM_TO_SPIN = 'Você precisa de %dx %s para girar.',
 
-	GIVE_REWARD_FAILURE = 'The item could not be delivered. Check if your backpack has space and relogin.',
-	GIVE_REWARD_SUCCESS = 'Congratulations, you received %dx %s.',
-	GIVE_REWARD_FOUND_RARE = '[Roulette]: Player %s found %dx %s, amazing.'
+	GIVE_REWARD_FAILURE = 'O item não pôde ser entregue. Verifique se sua mochila tem espaço e faça login novamente.',
+	GIVE_REWARD_SUCCESS = 'Parabéns, você recebeu %dx %s.',
+	GIVE_REWARD_FOUND_RARE = '[Roleta]: O jogador %s encontrou %dx %s, incrível.'
 }
