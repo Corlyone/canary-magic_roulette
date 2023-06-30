71aa8a92f2418958c3b4342878909b1640d3d35b
Nestor Ortega (NvSo) <91865189+Corlyone@users.noreply.github.com>
2023-06-30 11:30:03 -0500

    Update strings.lua
    Fix Strings

--- magic_roulette/lib/core/strings.lua
+++ magic_roulette/lib/core/strings.lua
@@ -1,21 +1,22 @@
 --[[
 	Description: This file is part of Roulette System (refactored)
-	Author: Lyµ
-	Discord: Lyµ#8767
+	Author: LyÂµ
+	Discord: LyÂµ#8767
 	Adaptado para o Canary:  NvSo#4349
 ]]
 
 return {
-	LOAD_CHANCE_MINMAX_WARNING = 'O item com o ID %d não pode ser menor que 0.01%% ou maior que 100%%.',
-	PRECISE_DROP_WARNING = '%d não é uma queda precisa, resultado: %s%%',
+	LOAD_CHANCE_MINMAX_WARNING = 'O item com o ID %d n\195\163o pode ser menor que 0.01%% ou maior que 100%%.',
+	PRECISE_DROP_WARNING = '%d n\195\163o \195\169 uma queda precisa, resultado: %s%%',
 
-	SLOT_NOT_IMPLEMENTED_YET = 'Slot ainda não implementado.',
+	SLOT_NOT_IMPLEMENTED_YET = 'Slot ainda n\195\163o implementado.',
 
 	WAIT_TO_SPIN = 'Aguarde para girar.',
 	GENERATE_REWARD_FAILURE = 'Algo deu errado, entre em contato com o administrador.',
-	NEEDITEM_TO_SPIN = 'Você precisa de %dx %s para girar.',
+	NEEDITEM_TO_SPIN = 'Voc\195\170 precisa de %dx %s para girar.',
 
-	GIVE_REWARD_FAILURE = 'O item não pôde ser entregue. Verifique se sua mochila tem espaço e faça login novamente.',
-	GIVE_REWARD_SUCCESS = 'Parabéns, você recebeu %dx %s.',
-	GIVE_REWARD_FOUND_RARE = '[Roleta]: O jogador %s encontrou %dx %s, incrível.'
+	GIVE_REWARD_FAILURE = 'O item n\195\163o p\195\180de ser entregue. Verifique se sua mochila tem espa\195\167o e fa\195\167a login novamente.',
+	GIVE_REWARD_SUCCESS = 'Parab\195\169ns, voc\195\170 recebeu %dx %s.',
+	GIVE_REWARD_FOUND_RARE = '[Roleta]: O jogador %s encontrou %dx %s, incr\195\173vel.'
+
 }
